2fbacdaa9905ffcb0a3705fef7a0d5b06f7b2573
1804351 <you@example.com>
2020-12-19 00:13:06 +0530

    weather app

---
+++ weather-app/src/components/Weather.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Container } from "@material-ui/core";
+
+import WeatherCard from "./WeatherCard";
+
+const useStyles = makeStyles((theme) => ({
+    layout: {
+        marginTop: "20px",
+    },
+}));
+
+const AppLayout = (props) => {
+    const classes = useStyles();
+    const { currentWeather, forecast, icon, recommendation } = props;
+
+    //NOTE: xs={12} means on small devices stretch to device width on bigger devices be smaller
+    return (
+        <Container className={classes.layout}>
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <WeatherCard
+                        currentWeather={currentWeather}
+                        forecast={forecast}
+                        icon={icon}
+                        recommendation={recommendation}
+                    />
+                </Grid>
+            </Grid>
+        </Container>
+    );
+};
+
+export default AppLayout;
